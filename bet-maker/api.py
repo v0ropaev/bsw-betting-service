33b817a954fa77e608a428a6164b7c4590060ed2
Dmitry Voropaev <workerv0ropaev@yandex.ru>
2024-11-07 11:43:04 +0300

    added api routes

--- bet-maker/api.py
+++ bet-maker/api.py
@@ -1,5 +1,25 @@
+from typing import List
+
 from fastapi import APIRouter
+from schemas import Event, Bet, BetCreateMessage
+from services import processing_get_events, processing_get_bets, processing_create_bet
 
 router = APIRouter()
 
 
+@router.get("/event")
+async def get_events() -> List[Event]:
+    result = await processing_get_events()
+    return result
+
+
+@router.post("/create_bet")
+async def create_bet(bet_data: Bet) -> BetCreateMessage:
+    result = await processing_create_bet(bet_data)
+    return result
+
+
+@router.get("/bets")
+async def get_bets() -> List[Bet]:
+    result = await processing_get_bets()
+    return result
